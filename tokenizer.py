f3db475caefdb913ad4e733048b08972329b33d1
Yuyuan <115848824+Yuyuan12138@users.noreply.github.com>
2024-07-18 10:48:27 +0800

    update

--- tokenizer.py
+++ tokenizer.py
@@ -31,40 +31,11 @@ class Tokenizer:
 tokenizer = Tokenizer().tokenizer
     
 if __name__ == "__main__":
-    # tokenizer = Tokenizer().tokenizer
-    # prompt = "the answer to the ultimate question of life, the universe, and everything is "
-    # tokens = [128000] + tokenizer.encode(prompt)
-    # print(tokens)
-    
-    # tokens = paddle.to_tensor(tokens)
-    # prompt_split_as_tokens = [tokenizer.decode([token.item()]) for token in tokens]
-    # print(prompt_split_as_tokens)
-    llama_text = """
-<|endoftext|>The llama (/ˈlɑːmə/; Spanish pronunciation: [ˈʎama] or [ˈʝama]) (Lama glama) is a domesticated South American camelid, widely used as a meat and pack animal by Andean cultures since the pre-Columbian era.
-Llamas are social animals and live with others as a herd. Their wool is soft and contains only a small amount of lanolin.[2] Llamas can learn simple tasks after a few repetitions. When using a pack, they can carry about 25 to 30% of their body weight for 8 to 13 km (5–8 miles).[3] The name llama (in the past also spelled "lama" or "glama") was adopted by European settlers from native Peruvians.[4]
-The ancestors of llamas are thought to have originated from the Great Plains of North America about 40 million years ago, and subsequently migrated to South America about three million years ago during the Great American Interchange. By the end of the last ice age (10,000–12,000 years ago), camelids were extinct in North America.[3] As of 2007, there were over seven million llamas and alpacas in South America and over 158,000 llamas and 100,000 alpacas, descended from progenitors imported late in the 20th century, in the United States and Canada.[5]
-<|fim_prefix|>In Aymara mythology, llamas are important beings. The Heavenly Llama is said to drink water from the ocean and urinates as it rains.[6] According to Aymara eschatology,<|fim_suffix|> where they come from at the end of time.[6]<|fim_middle|> llamas will return to the water springs and ponds<|endofprompt|>
-""".strip()
-    special_tokens = {
-    '<|endoftext|>': 100257,
-    '<|fim_prefix|>': 100258,
-    '<|fim_middle|>': 100259,
-    '<|fim_suffix|>': 100260,
-    '<|endofprompt|>': 100276
-}
-    GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
-    LLAMA3_SPLIT_PATTERN = r"""(?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n]*|\s*[\r\n]+|\s+(?!\S)|\s+"""
-    # tokenizer = RegexTokenizer(GPT4_SPLIT_PATTERN)
     tokenizer = Tokenizer().tokenizer
-    # tokenizer = bpe.RegexTokenizer(LLAMA3_SPLIT_PATTERN)
-    # tokenizer.register_special_tokens(special_tokens=special_tokens)
-    prompt = "hello, world"
-    # print(prompt)
-    # print(tokenizer.encode(prompt))
-    # tokens = paddle.to_tensor(tokenizer.encode(prompt, set(special_tokens)))
+    prompt = "the answer to the ultimate question of life, the universe, and everything is "
     tokens = [128000] + tokenizer.encode(prompt)
     print(tokens)
+    
     tokens = paddle.to_tensor(tokens)
     prompt_split_as_tokens = [tokenizer.decode([token.item()]) for token in tokens]
     print(prompt_split_as_tokens)
-    
